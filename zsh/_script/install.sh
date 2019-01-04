034aa627e3befebdb4635cee40449baad66c513b
PruDyl <dylan@iprotego.com>
2019-01-04 10:17:01 +0100

    Add oh-my-zsh installation + update README

---
+++ zsh/_script/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "[-][oh-my-zsh] Begin installation ..."
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "[+][oh-my-zsh] installation complete."
