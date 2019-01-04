3393108f6b46a3e2f8eaf9896c8ca201e06590de
PruDyl <dylan@iprotego.com>
2019-01-04 10:32:18 +0100

    update zsh config

--- zsh/_script/install.sh
+++ zsh/_script/install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-echo "[-][oh-my-zsh] Begin installation ..."
+echo "[-][zsh] Begin installation ..."
+
+# we create symbolic links to the config files if they don't already exist, else rename as FILE.DATE.BAK
+DATE=$(date "+%Y.%m.%d-%H.%M.%S")
+
+if [ -e ~/.zshrc ] ; then
+    echo "Backup previous files"
+    mv ~/.zshrc ~/.zshrc.$DATE.bak && echo "Rename ~/.zshrc as ~/.zshrc.$DATE.bak"
+fi
+echo "Symbolic link"
+ln -s "`pwd`/.zshrc" ~/.zshrc
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "[+][oh-my-zsh] installation complete."
+echo "[+][zsh] installation complete."
